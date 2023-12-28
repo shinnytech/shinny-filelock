022b79b6fb51df6d5d109598ace5298cfd19db2e
shinny-taojiachun <taojiachun@shinnytech.com>
2023-12-28 16:53:13 +0800

    fix test

--- tests/test_flock.py
+++ tests/test_flock.py
@@ -12,31 +12,39 @@ class TestSimple(unittest.TestCase):
 
     def test_non_blocking_lock(self):
         local_path = "/tmp/test.lock"
-        with flocked(local_path):
+        with flocked(local_path, create_file=True):
             try:
-                with flocked(local_path):
+                with flocked(local_path, create_file=True):
                     pass
             except Exception as e:
                 self.assertEqual(e.__class__, BlockingIOError)
 
     def test_blocking_lock(self):
         local_path = "/tmp/test-block.lock"
-        with flocked(local_path, blocking=True):
+        with flocked(local_path, blocking=True, create_file=True):
             try:
                 with timeout(2):
-                    with flocked(local_path, blocking=True):
+                    with flocked(local_path, blocking=True, create_file=True):
                         pass
             except Exception as e:
                 self.assertEqual(e.__class__, TimeoutError)
 
     def test_blocking_with_non_blocking_lock(self):
         local_path = "/tmp/test-mix.lock"
-        with flocked(local_path, blocking=True):
+        with flocked(local_path, blocking=True, create_file=True):
             try:
-                with flocked(local_path, blocking=False):
+                with flocked(local_path, blocking=False, create_file=True):
                     pass
             except Exception as e:
                 self.assertEqual(e.__class__, BlockingIOError)
+
+    def test_file_not_exists(self):
+        local_path = "/tmp/test-not-exists.lock"
+        try:
+            with flocked(local_path, blocking=True):
+                pass
+        except Exception as e:
+            self.assertEqual(e.__class__, FileNotFoundError)
 
 
 if __name__ == '__main__':
