bd8a21864c5f7589d1085631f91dce5e479e0163
shinny-taojiachun <taojiachun@shinnytech.com>
2024-08-28 11:36:35 +0800

    feat(stickybits): fix sticky bits and add inheritable option

--- src/shinny_filelock/_flockd.py
+++ src/shinny_filelock/_flockd.py
@@ -4,21 +4,28 @@ from contextlib import contextmanager
 
 
 @contextmanager
-def flocked(path, blocking=True, create_file=False, shared=False):
+def flocked(path, blocking=True, create_file=False, shared=False, inheritable=False):
     """
     :param path: file path
     :param blocking: blocking lock, default True
     :param create_file: create file if not exists, default False
     :param shared: shared lock, default False
+    :param inheritable: inherit lock after exec, default False
     """
     fd = -1
+    fd_flags = os.O_RDONLY | os.O_NOCTTY
+    fd_flags_with_create = fd_flags | os.O_CREAT
     try:
-        fd_flags = os.O_RDONLY | os.O_NOCTTY
-        if create_file:
-            fd_flags |= os.O_CREAT
-        fd = os.open(path, fd_flags)
+        # when stick bits set, os.O_CREAT will raise PermissionError
+        try:
+            fd = os.open(path, fd_flags_with_create if create_file else fd_flags)
+        except PermissionError:
+            # maybe file exists and sits in a directory with sticky bits
+            fd = os.open(path, fd_flags)
         if fd == -1:
             raise ValueError()
+        # default fd in non-inheritable. See PEP 446: https://peps.python.org/pep-0446/
+        os.set_inheritable(fd, inheritable)
         flags = fcntl.LOCK_SH if shared else fcntl.LOCK_EX
         if not blocking:
             flags |= fcntl.LOCK_NB
