44c195f0064641a3d62010e055c63ff22a514fee
chengzhi <chengzhi@shinnytech.com>
2024-09-30 13:54:25 +0800

    fix lock still been held in forked subprocess due to open fd

--- src/shinny_filelock/_flockd.py
+++ src/shinny_filelock/_flockd.py
@@ -12,26 +12,30 @@ def flocked(path, blocking=True, create_file=False, shared=False, inheritable=Fa
     :param shared: shared lock, default False
     :param inheritable: inherit lock after exec, default False
     """
-    fd = -1
     fd_flags = os.O_RDONLY | os.O_NOCTTY
     fd_flags_with_create = fd_flags | os.O_CREAT
+    # when stick bits set, os.O_CREAT will raise PermissionError
     try:
-        # when stick bits set, os.O_CREAT will raise PermissionError
-        try:
-            fd = os.open(path, fd_flags_with_create if create_file else fd_flags)
-        except PermissionError:
-            # maybe file exists and sits in a directory with sticky bits
-            fd = os.open(path, fd_flags)
-        if fd == -1:
-            raise ValueError()
+        fd = os.open(path, fd_flags_with_create if create_file else fd_flags)
+    except PermissionError:
+        # maybe file exists and sits in a directory with sticky bits
+        fd = os.open(path, fd_flags)
+    if fd == -1:
+        raise ValueError()
+    try:
         # default fd in non-inheritable. See PEP 446: https://peps.python.org/pep-0446/
         os.set_inheritable(fd, inheritable)
         flags = fcntl.LOCK_SH if shared else fcntl.LOCK_EX
         if not blocking:
             flags |= fcntl.LOCK_NB
         fcntl.flock(fd, flags)
-        yield
+        try:
+            yield
+        finally:
+            # early unlock in case someone is holding the fd(eg: fork without exec)
+            # lock may also released due to fd closure when inheritable is true
+            fcntl.flock(fd, fcntl.LOCK_UN)
     finally:
-        if fd != -1:
-            # close fd will release lock automatically
-            os.close(fd)
+        # lock should already released
+        # close fd to avoid resource leak
+        os.close(fd)
